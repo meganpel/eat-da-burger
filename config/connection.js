eba9bce19b3d71479f2dc4ba6c108f20f7179ddb
Megan Pellegrino-Zubricky <pellegrino.megan@gmail.com>
2018-08-18 21:43:48 -0400

    added jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "",
-  port: 0,
-  user: "",
-  password: "",
-  database: ""
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "",
+    port: 0,
+    user: "",
+    password: "",
+    database: ""
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
